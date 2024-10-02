a40bff708f7ebdef20b6dba2492b4b6842602cf6
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-02 17:08:32 +0300

    Add files via upload

---
+++ ОС/3+/OS-03-01/OS-03-01/OS-03-01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    DWORD processID = GetCurrentProcessId();
+    cout << "Идентификатор процесса: " << processID << endl;
+
+    while (true) {
+        cout << "Процесс работает...\nИдентификатор: " << processID << endl;
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+
+    return 0;
+}
