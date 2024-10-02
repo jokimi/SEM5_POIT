dde65f1824df54f4ce0084376ba0fae190422daf
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-02 17:18:23 +0300

    Add files via upload

---
+++ ОС/3+/OS-03-03/OS-03-03/OS-03-03.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+using namespace std;
+
+void listProcesses() {
+    // Получаем дескриптор для создания снимка процессов
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) {
+        cerr << "Ошибка создания снимка процессов: " << GetLastError() << endl;
+        return;
+    }
+
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
+
+    // Получаем первый процесс
+    if (Process32First(hSnapshot, &pe)) {
+        do {
+            wcout << L"Процесс: " << pe.szExeFile << L" (PID: " << pe.th32ProcessID << L")" << endl;
+        } while (Process32Next(hSnapshot, &pe));
+    }
+    else {
+        cerr << "Ошибка получения информации о процессе: " << GetLastError() << endl;
+    }
+
+    // Закрываем дескриптор
+    CloseHandle(hSnapshot);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    wcout << L"Список выполняющихся процессов:" << endl;
+    listProcesses();
+    return 0;
+}
