4769cb4d42ecfbfc8a8f1f96bc237e3294566eff
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-02 17:14:30 +0300

    Add files via upload

---
+++ ОС/3+/OS-03-04/OS-03-04/OS-03-04.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+int main() {
+#ifdef _WIN32
+    DWORD pid = GetCurrentProcessId();
+    printf("OS-03-04 ID: %lu\n", pid);
+#else
+    pid_t pid = getpid();
+    printf("OS-03-04 ID: %d\n", pid);
+#endif
+
+    for (int i = 0; i < 100; i++) {
+        printf("Iteration %d\n", i + 1);
+#ifdef _WIN32
+        Sleep(1000); // Задержка в миллисекундах для Windows
+#else
+        sleep(1);    // Задержка в секундах для Linux
+#endif
+    }
+
+    return 0;
+}
---
+++ ОС/3+/OS-03-04/build/OS-03-04/CMakeFiles/OS-03-04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS-03-04.dir/OS-03-04.c.o"
+  "OS-03-04"
+  "OS-03-04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS-03-04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
