972db736ac8e8a134143ef0c89242c29cf099637
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-07 12:08:06 +0300

    Add files via upload

---
+++ ОС/4+/OS04-04-WinAPI/OS04-04-WinAPI/OS04-04-WinAPI.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+DWORD WINAPI ThreadZed(LPVOID lpParam) {
+    for (int i = 0; i < 10; i++) {
+        printf(" (Z-%d) ", GetCurrentThreadId());
+        Sleep(1000);
+    }
+    printf(" Поток Z завершается\n");
+    return 0;
+}
+
+DWORD WINAPI ThreadWithParam(LPVOID lpParam) {
+    for (int i = 0; i < 20; i++) {
+        printf(" (%s-%d) ", (char*)lpParam, GetCurrentThreadId());
+        Sleep(1000);
+    }
+    printf(" Поток %s завершается\n", (char*)lpParam);
+    return 0;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    HANDLE t1, t1a, t1b;
+
+    // Создание потоков
+    t1 = CreateThread(NULL, 0, ThreadZed, NULL, 0, NULL);
+    t1a = CreateThread(NULL, 0, ThreadWithParam, "Елизавета", 0, NULL);
+    t1b = CreateThread(NULL, 0, ThreadWithParam, "Козека", 0, NULL);
+
+    // Главный поток работает 5 секунд
+    for (int i = 0; i < 5; i++) {
+        printf(" (*-%d) ", GetCurrentThreadId());
+        Sleep(1000);
+    }
+    printf("Главный поток завершается\n");
+
+    WaitForSingleObject(t1, INFINITE);
+    WaitForSingleObject(t1a, INFINITE);
+    WaitForSingleObject(t1b, INFINITE);
+
+    CloseHandle(t1);
+    CloseHandle(t1a);
+    CloseHandle(t1b);
+
+    return 0;
+}
---
+++ ОС/4+/OS04-04/OS04-04/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    // Поток Z работает 10 секунд
+    static void ThreadZed()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(" (Z-{0}) ", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine(" Поток Z завершается ");
+    }
+
+    // Поток работает 20 секунд, параметр - строка-идентификатор
+    static void ThreadWithParam(object o)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(" ({0}-{1}) ", o.ToString(), Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine(" Поток {0} завершается ", o);
+    }
+
+    static void Main(string[] args)
+    {
+        var t1 = new Thread(ThreadZed);
+        var t1a = new Thread(ThreadWithParam);
+        var t1b = new Thread(ThreadWithParam);
+
+        t1.IsBackground = true; // Поток Z
+        t1a.IsBackground = false; // Поток с именем "Имя"
+        t1b.IsBackground = true; // Поток с фамилией "Фамилия"
+
+        t1.Start();
+        t1a.Start("Елизавета");
+        t1b.Start("Козека");
+
+        // Главный поток работает 5 секунд
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(" (*-{0}) ", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("\n Главный поток завершается");
+    }
+}
