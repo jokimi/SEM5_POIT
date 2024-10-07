7c1002763875d0023d805ffecfabd7233a2c887e
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-07 12:12:15 +0300

    Add files via upload

---
+++ ОС/4+/OS04-05/OS04-05/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static double MySleep(int ms)
+    {
+        double sum = 0, temp;
+
+        // Преобразование миллисекунд в такты
+        int endTicks = Environment.TickCount + ms;
+
+        while (Environment.TickCount < endTicks)
+        {
+            for (int t = 0; t < 100; t++)
+            {
+                temp = 0.711 + (double)t / 10000.0;
+                double a, b, c, d, e, nt;
+
+                for (int k = 0; k < 5500; k++)
+                {
+                    nt = temp - k / 27000.0;
+                    a = Math.Sin(nt);
+                    b = Math.Cos(nt);
+                    c = Math.Cos(nt / 2.0);
+                    d = Math.Sin(nt / 2);
+                    e = Math.Abs(1.0 - a * a - b * b) + Math.Abs(1.0 - c * c - d * d);
+                    sum += e; // Нагружение процессора
+                }
+            }
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Начинается выполнение MySleep(10000)...");
+        double result = MySleep(10000); // 10 секунд
+        Console.WriteLine("Выполнено. Результат: " + result);
+    }
+}
---
+++ ОС/4+/OS04-06/OS04-06/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static int Count = 0;
+    static readonly object lockObject = new object(); // Для синхронизации
+
+    static void WorkThread()
+    {
+        for (int i = 0; i < 5000000; ++i)
+        {
+            lock (lockObject) // Блокировка для обеспечения безопасности
+            {
+                Count++;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread[] threads = new Thread[20];
+
+        // Запуск 20 потоков
+        for (int i = 0; i < 20; ++i)
+        {
+            threads[i] = new Thread(WorkThread);
+            threads[i].Start();
+        }
+
+        // Ожидание завершения всех потоков
+        for (int i = 0; i < 20; ++i)
+        {
+            threads[i].Join();
+        }
+
+        Console.WriteLine("Результат Count: " + Count);
+        Console.WriteLine("Ожидаемое значение: " + (20 * 5000000));
+    }
+}
