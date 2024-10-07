fd9f458d943925bcd4a531802b305d21ea7468dc
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-07 12:04:39 +0300

    Add files via upload

---
+++ ОС/4+/OS04-01/OS04-01/OS04-01.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+    DWORD processId = GetCurrentProcessId();
+    DWORD threadId = GetCurrentThreadId();
+
+    std::cout << "Process ID: " << processId << std::endl;
+    std::cout << "Thread ID: " << threadId << std::endl;
+
+    for (int i = 0; i < 100; ++i) {
+        Sleep(1000);
+        std::cout << "Iteration: " << (i + 1) << std::endl;
+    }
+
+    return 0;
+}
