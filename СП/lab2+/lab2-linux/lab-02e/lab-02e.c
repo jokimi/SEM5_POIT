15ad2012f8d68b41a76091e2de7aed45e72449fb
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-18 12:47:17 +0300

    Add files via upload

---
+++ СП/lab2+/lab2-linux/lab-02e/lab-02e.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid1, pid2;
+
+    // Создание первого дочернего процесса
+    pid1 = fork();
+    if (pid1 < 0) {
+        perror("Failed to fork first child");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0) {
+        // Первый дочерний процесс
+        char* args[] = { "/mnt/d/bstu/5 sem/сп/лабы/lab2-linux/build/lab-02x-linux/lab-02x-linux", "10", NULL }; // Передаем 10 итераций
+        execvp(args[0], args);
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Создание второго дочернего процесса
+    pid2 = fork();
+    if (pid2 < 0) {
+        perror("Failed to fork second child");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid2 == 0) {
+        // Второй дочерний процесс
+        setenv("ITER_NUM", "20", 1); // Установка переменной окружения
+        char* args[] = { "/mnt/d/bstu/5 sem/сп/лабы/lab2-linux/build/lab-02x-linux/lab-02x-linux", NULL }; // Не передаем аргументы
+        execvp(args[0], args);
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Родительский процесс ждет завершения дочерних процессов
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    printf("Both child processes have completed.\n");
+    return EXIT_SUCCESS;
+}
---
+++ СП/lab2+/lab2-linux/lab-02x-linux/lab-02x-linux.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+    int iterations;
+
+    // Проверка наличия аргумента командной строки
+    if (argc > 1) {
+        iterations = atoi(argv[1]);
+    }
+    else {
+        // Проверка переменной окружения ITER_NUM
+        char* envVar = getenv("ITER_NUM");
+        if (envVar) {
+            iterations = atoi(envVar);
+            printf("Using ITER_NUM from environment: %s\n", envVar);
+        }
+        else {
+            fprintf(stderr, "Error: ITER_NUM is not set.\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    printf("Iterations: %d\n", iterations);
+    for (int i = 0; i < iterations; i++) {
+        printf("Iteration %d, PID: %d\n", i + 1, getpid());
+        usleep(500000); // Пауза на 500 мс
+    }
+
+    return EXIT_SUCCESS;
+}
