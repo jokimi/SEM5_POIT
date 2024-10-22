4cdc4bdb06135aefcb580351ac2934861c2be863
keikenny <118848006+jokimi@users.noreply.github.com>
2024-10-22 17:11:54 +0300

    Add files via upload

---
+++ ОС/5+/OS05-04/OS05-04/OS05-04.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <thread>
+#include <sched.h>
+#include <unistd.h>
+#include <vector>
+
+int main() {
+    // Получение идентификатора текущего процесса
+    pid_t process_id = getpid();
+
+    // Получение идентификатора текущего потока
+    std::thread::id thread_id = std::this_thread::get_id();
+
+    // Получение приоритета (nice) текущего потока
+    int nice_value = nice(0); // 0 - для получения текущего значения nice
+
+    // Получение списка доступных процессоров
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    sched_getaffinity(0, sizeof(cpu_set_t), &cpuset);
+
+    std::vector<int> available_cpus;
+    for (int i = 0; i < CPU_SETSIZE; ++i) {
+        if (CPU_ISSET(i, &cpuset)) {
+            available_cpus.push_back(i);
+        }
+    }
+
+    // Вывод информации
+    std::cout << "Идентификатор текущего процесса: " << process_id << std::endl;
+    std::cout << "Идентификатор текущего (main) потока: " << thread_id << std::endl;
+    std::cout << "Приоритет (nice) текущего потока: " << nice_value << std::endl;
+    std::cout << "Номера доступных процессоров: ";
+    for (const int& cpu : available_cpus) {
+        std::cout << cpu << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ ОС/5+/OS05-05/OS05-05/OS05-05.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    printf("Начало выполнения программы OS05_05.\n");
+    printf("Идентификатор процесса: %d\n", getpid());
+
+    int i = 1;
+    while (i <= 1000) {
+        usleep(100000); // 100 мс
+        i++;
+    }
+
+    return 0; // Эта строка никогда не будет достигнута
+}
