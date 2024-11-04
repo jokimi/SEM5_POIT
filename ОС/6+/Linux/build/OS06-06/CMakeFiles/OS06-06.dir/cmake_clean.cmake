c22b725e51c2d688e08d9d9345f963b3e12ee285
keikenny <118848006+jokimi@users.noreply.github.com>
2024-11-04 12:17:03 +0300

    Add files via upload

---
+++ ОС/6+/Linux/build/OS06-06/CMakeFiles/OS06-06.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS06-06.dir/OS06-06.cpp.o"
+  "OS06-06"
+  "OS06-06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS06-06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
