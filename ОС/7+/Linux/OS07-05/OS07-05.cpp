59d3f307612c20aa34ac7420a85eebccd57d4d9e
keikenny <118848006+jokimi@users.noreply.github.com>
2024-11-14 23:47:19 +0300

    Add files via upload

---
+++ ОС/7+/Linux/OS07-05/OS07-05.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t rawtime;
+    struct tm* info;
+    time(&rawtime);
+    info = localtime(&rawtime);
+    printf("%02d.%02d.%04d %02d:%02d:%02d\n",
+        info->tm_mday, info->tm_mon + 1, info->tm_year + 1900,
+        info->tm_hour, info->tm_min, info->tm_sec);
+    return 0;
+}
---
+++ ОС/7+/Linux/OS07-06/OS07-06.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main() {
+    clock_t start_time, current_time;
+    int counter = 0;
+    start_time = clock();
+    while (1) {
+        counter++;
+        current_time = clock();
+        if ((double)(current_time - start_time) / CLOCKS_PER_SEC >= 2.0) {
+            printf("Iterations: %d\n", counter);
+            printf("Load time: %d\n", current_time - start_time);
+            start_time = current_time;
+            break;
+        }
+    }
+    return 0;
+}
