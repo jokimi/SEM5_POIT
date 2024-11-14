c02c65123be3a2faa5ce797975c775509e6d6f6d
keikenny <118848006+jokimi@users.noreply.github.com>
2024-11-14 23:49:09 +0300

    Add files via upload

---
+++ ОС/7+/Windows/OS07-01/OS07-01.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t rawtime;
+    struct tm info;
+    char buffer[80];
+    time(&rawtime);
+    localtime_s(&info, &rawtime);
+    strftime(buffer, sizeof(buffer), "%d.%m.%Y %H:%M:%S", &info);
+    printf("%s\n", buffer);
+    return 0;
+}
---
+++ ОС/7+/Windows/OS07-02/OS07-02.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <windows.h>
+
+using namespace std;
+
+int _counter = 0;
+
+int getCurrentCounter() {
+    return _counter;
+}
+
+void main() {
+    auto startTime = std::chrono::steady_clock::now();
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    bool fiveSec = true, tenSec = true, fifteenSec = true;
+    while (true) {
+        auto currentTime = std::chrono::steady_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+        if (elapsedTime >= 5 && fiveSec) {
+            std::cout << "Значение счетчика через 5 секунд: " << getCurrentCounter() << std::endl;
+            fiveSec = false;
+        }
+        if (elapsedTime >= 10 && tenSec) {
+            std::cout << "Значение счетчика через 10 секунд: " << getCurrentCounter() << std::endl;
+            tenSec = false;
+        }
+        if (elapsedTime >= 15 && fifteenSec) {
+            std::cout << "Прошло 15 секунд. Завершаем работу." << std::endl;
+            std::cout << "Итоговое значение счётчика: " << getCurrentCounter() << std::endl;
+            break;
+        }
+        _counter++;
+    }
+}
