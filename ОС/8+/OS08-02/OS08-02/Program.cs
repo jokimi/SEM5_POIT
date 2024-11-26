9e0ba495e1627a99677329f8b5c7e89861bd8f0f
keikenny <118848006+jokimi@users.noreply.github.com>
2024-11-26 20:30:07 +0300

    Add files via upload

---
+++ ОС/8+/OS08-02/OS08-02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("OS08_02: Создание объектов по 128 МБ каждые 5 секунд.");
+        Console.WriteLine("Platform Target: x86\n");
+
+        // Список для хранения ссылок на созданные объекты, чтобы предотвратить их сборку сборщиком мусора
+        var objectList = new System.Collections.Generic.List<byte[]>();
+
+        Thread memoryFillerThread = new Thread(() =>
+        {
+            Random random = new Random();
+            while (true)
+            {
+                foreach (var obj in objectList)
+                {
+                    for (int i = 0; i < obj.Length; i++)
+                    {
+                        obj[i] = (byte)random.Next(256);
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        });
+        memoryFillerThread.IsBackground = true;
+        memoryFillerThread.Start();
+
+        int counter = 0;
+        while (true)
+        {
+            counter++;
+
+            byte[] bigObject = new byte[128 * 1024 * 1024];
+            objectList.Add(bigObject);
+
+            long memoryUsage = Process.GetCurrentProcess().PrivateMemorySize64;
+            long workingSet = Process.GetCurrentProcess().WorkingSet64;
+
+            Console.WriteLine($"[{counter}] Использование памяти: {memoryUsage / (1024 * 1024)} MB, Рабочий набор: {workingSet / (1024 * 1024)} MB");
+
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ ОС/8+/OS08-03/OS08-03/OS08-03.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <unistd.h>
+
+int global_var = 0;
+int uninit_global_var;
+
+void long_loop() {
+    for (long long i = 0; i < 1e10; ++i) {
+        if (i % 100000000 == 0) {
+            global_var++;
+            std::cout << "Iteration: " << i << ", global_var: " << global_var << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+}
+
+int main() {
+    std::cout << "OS08_03: Длинный цикл. PID = " << getpid() << "\n";
+    long_loop();
+    return 0;
+}
